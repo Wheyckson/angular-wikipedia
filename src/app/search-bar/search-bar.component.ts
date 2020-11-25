00dff9bc37430890d09ef1f434a14dc4a2936732
Wheyckson Lopes <57054127+Wheyckson@users.noreply.github.com>
2020-11-25 18:38:30 -0300

    wikisearch

---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+  @Output() submitted = new EventEmitter<string>();
+  term = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onInput(value: string) {
+    this.term = value;
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    this.submitted.emit(this.term);
+  }
+
+}
